999dd7aca370ee7d938566fb642dac64f553157c
AkshaySharma-BharatPe <akshay.sharma@bharatpe.com>
2022-08-24 10:46:57 +0530

    v1

---
+++ index.js
@@ -0,0 +1,30 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+const exec = require("@actions/exec");
+const { Octokit } = require("@octokit/rest");
+
+
+const main = async () => {
+    const inputs = {
+        token: core.getInput("token"),
+    };
+
+    if (!pullRequest) {
+        core.error("This action only works on pull_request events");
+        return;
+      }
+  
+    const { number: issueNumber } = pullRequest;
+    const { full_name: repoFullName } = repository;
+    const [owner, repo] = repoFullName.split("/");
+  
+    const octokit = new Octokit({
+        auth: inputs.token,
+    });
+
+    console.log(inputs.token, issueNumber, repoFullName, owner, repo);
+
+    await exec.exec(`git branch`);
+};
+
+main();
