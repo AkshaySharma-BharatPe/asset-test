a96dc972b5d002c2dbfa9f533ab39e13bd4d93d4
AkshaySharma-BharatPe <akshay.sharma@bharatpe.com>
2022-08-24 13:29:14 +0530

    4.9

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ const main = async () => {
       auth: inputs.token,
     });
 
-    const res = await exec.exec(`find src/assets -name "*.jpeg" -type f -size +100k -exec ls -lh > output.txt \;`);
+    const res = await exec.exec(`find src/assets -name "*.jpeg" -type f -size +100k -exec ls -lh \; -exec wc -lines`);
     console.log(res);
 
     const assetsMoreThanThrashold = await getAssetsCount();
