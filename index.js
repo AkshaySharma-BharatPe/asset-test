ba0ed288b4d1c60bbc06d3cc78d8b964a50e5724
AkshaySharma-BharatPe <akshay.sharma@bharatpe.com>
2022-08-24 16:12:55 +0530

    version 2

--- index.js
+++ index.js
@@ -1,16 +1,25 @@
 const core = require("@actions/core");
 const github = require("@actions/github");
-const exec = require("@actions/exec");
 const { Octokit } = require("@octokit/rest");
+const exec = require("@actions/exec");
 
-const main = async () => {
+async function run() {
+  function bytesToSize(bytes) {
+    const sizes = ["Bytes", "KB", "MB", "GB", "TB"];
+    if (bytes === 0) return "0 Byte";
+    const i = parseInt(Math.floor(Math.log(Math.abs(bytes)) / Math.log(1024)));
+    return (bytes / Math.pow(1024, i)).toFixed(2) + " " + sizes[i];
+  }
+
   try {
-
     const inputs = {
       token: core.getInput("token"),
-      target_folder: core.getInput("target_folder"),
-      thrashold_size: core.getInput("thrashold_size")
-    };
+      install_command: core.getInput("install_command"),
+      build_command: core.getInput("build_command"),
+      build_path: core.getInput("build_path"),
+      base_branch: core.getInput("base_branch"),
+      head_branch: core.getInput("head_branch"),
+  };
 
     const {
       payload: { pull_request: pullRequest, repository },
@@ -29,45 +38,74 @@ const main = async () => {
       auth: inputs.token,
     });
 
+    await exec.exec(`git fetch`);
+    
+    const branches = [inputs.head_branch, inputs.base_branch];
+    const branchesStats = [];
+    const branchesHeading = [];
 
-    let myOutput = '';
-    let myError = '';
-    const options = {};
-    options.listeners = {
-      stdout: (data) => {
-        myOutput += data.toString();
-      },
-      stderr: (data) => {
-        myError += data.toString();
-      }
-    };
+    for (let item of branches) {
+      await exec.exec(`git checkout ${item}`);
+      await exec.exec(inputs.install_command);
+      await exec.exec(inputs.build_command);
 
-    await exec.exec(`find ${target_folder} -type f  ! -regex  '.*\(png\|gif\|jpg\|svg\|jpeg\)$' -size +${thrashold_size}k -exec ls -lh {} \;`, null, options); 
-    const arrayOutput = myOutput.split("\n");
-    const count = arrayOutput.length -1;
+      core.setOutput(
+        "Building repo completed - 1st @ ",
+        new Date().toTimeString()
+      );
 
-    const successBody = ` Woohooo :rocket: !!! Congratulations, your all assets are less than 100Kb.`
-    console.log(count > 0);
-    if(count > 0) {
-      const errorBody = `Oops :eyes: !!! You have ${count} assets with size more than 100Kb. Please optimize them.`
-      octokit.rest.issues.createComment({
-        owner,
-        repo,
-        issue_number: issueNumber,
-        body: errorBody,
+      const outputOptions = {};
+      let sizeCalOutput = "";
+
+      outputOptions.listeners = {
+        stdout: (data) => {
+          sizeCalOutput += data.toString();
+        },
+        stderr: (data) => {
+          sizeCalOutput += data.toString();
+        },
+      };
+
+      await exec.exec(`du ${inputs.build_path}`, null, outputOptions);
+      core.setOutput("size", sizeCalOutput);
+
+      const arrayOutput = sizeCalOutput.split("\n");
+
+      const arrOp = arrayOutput.map((item) => {
+        const i = item.split(/(\s+)/);
+        branchesHeading.push(`${i[2]}`);
+        return parseInt(i[0]) * 1000;
       });
-    }else {
-      octokit.rest.issues.createComment({
-        owner,
-        repo,
-        issue_number: issueNumber,
-        body: successBody,
-      });
+      branchesStats.push(arrOp);
     }
 
+    const coverage = `|Files Type|New Stats (${
+      inputs.head_branch
+    })|Old Stats (${inputs.base_branch})|Differences (New - Old)|
+|-----|:-----:|:-----:|:-----:|
+|${branchesHeading[0]}|${bytesToSize(branchesStats[0][0])}|${bytesToSize(
+      branchesStats[1][0]
+    )}|${bytesToSize(branchesStats[0][0] - branchesStats[1][0])}|
+|${branchesHeading[1]}|${bytesToSize(branchesStats[0][1])}|${bytesToSize(
+      branchesStats[1][1]
+    )}|${bytesToSize(branchesStats[0][1] - branchesStats[1][1])}|
+|${branchesHeading[2]}|${bytesToSize(branchesStats[0][2])}|${bytesToSize(
+      branchesStats[1][2]
+    )}|${bytesToSize(branchesStats[0][2] - branchesStats[1][2])}|
+|${branchesHeading[3]}|${bytesToSize(branchesStats[0][3])}|${bytesToSize(
+      branchesStats[1][3]
+    )}|${bytesToSize(branchesStats[0][3] - branchesStats[1][3])}|
+`;
+
+    octokit.rest.issues.createComment({
+      owner,
+      repo,
+      issue_number: issueNumber,
+      body: coverage,
+    });
   } catch (error) {
     core.setFailed(error.message);
   }
-};
+}
 
-main();
+run();
