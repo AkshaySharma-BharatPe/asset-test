3e4daad8378ef5e81061145e5c418879a847592a
AkshaySharma-BharatPe <akshay.sharma@bharatpe.com>
2022-08-24 16:33:34 +0530

    version 2

--- index.js
+++ index.js
@@ -33,6 +33,7 @@ const main = async () => {
     let myOutput = '';
     let myError = '';
     const options = {};
+
     options.listeners = {
       stdout: (data) => {
         myOutput += data.toString();
@@ -43,19 +44,22 @@ const main = async () => {
     };
 
     await exec.exec(`find ${inputs.target_folder} -type f  ! -regex  '.*\(png\|gif\|jpg\|svg\|jpeg\)$' -size +${inputs.thrashold_size}k -exec ls -lh {} \;`, null, options); 
+
     const arrayOutput = myOutput.split("\n");
     const count = arrayOutput.length -1;
 
     const successBody = ` Woohooo :rocket: !!! Congratulations, your all assets are less than 100Kb.`
-    console.log(count > 0);
+    const errorBody = `Oops :eyes: !!! You have ${count} assets with size more than ${inputs.thrashold_size}Kb. Please optimize them.`
+
+
     if(count > 0) {
-      const errorBody = `Oops :eyes: !!! You have ${count} assets with size more than ${inputs.thrashold_size}Kb. Please optimize them.`
       octokit.rest.issues.createComment({
         owner,
         repo,
         issue_number: issueNumber,
         body: errorBody,
       });
+      core.setFailed('Invalid size assets exists !!!');
     }else {
       octokit.rest.issues.createComment({
         owner,
