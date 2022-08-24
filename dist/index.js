1a7918892f005a8b8f72d8412f79baa891441ea4
AkshaySharma-BharatPe <akshay.sharma@bharatpe.com>
2022-08-24 14:25:29 +0530

    4.9

--- dist/index.js
+++ dist/index.js
@@ -13302,7 +13302,7 @@ const main = async () => {
     });
 
 
-    const res = await exec.exec(`find ./src/assets/ -iname '*.gif' -o -iname '*.jpg' -o -iname '*.svg' -o -iname '*.jpeg' -o -iname '*.png' -type f -size +100k -exec ls -lh {} \; | wc -l`);
+    const res = await exec.exec(`find ./src/assets/ -iname '*.gif' -o -iname '*.jpg' -o -iname '*.svg' -o -iname '*.jpeg' -o -iname '*.png' -type f -size +100k -exec ls -lh {} \;`);
     console.log(res);
 
     const assetsMoreThanThrashold = await getAssetsCount();
